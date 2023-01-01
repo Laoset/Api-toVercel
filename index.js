02a9e9d6093c9695830559e28f82a1898c6a0686
Laoset <xlaospt2@gmail.com>
2023-01-01 20:30:58 -0300

    inicio

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const db = require('./api/db')
+
+// app.use(express.jso({ extended: false}));
+
+app.use("/api/db", db)
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, ()=> console.log (`Servidor en funcionamiento en el puerto ${PORT}`))

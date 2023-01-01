57c549d77a573a1b970ea28235257e31dc1fc2fd
Laoset <xlaospt2@gmail.com>
2023-01-01 20:36:47 -0300

    vercelFile

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require('express');
 const app = express();
 const db = require('./api/db')
 
-// app.use(express.jso({ extended: false}));
+app.use(express.json({ extended: false}));
 
 app.use("/api/db", db)
 
 const PORT = process.env.PORT || 8080;
-
 app.listen(PORT, ()=> console.log (`Servidor en funcionamiento en el puerto ${PORT}`))
